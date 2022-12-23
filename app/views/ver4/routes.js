0fe2e691a7225bf53076b61d4bd2b1ab2f53ce47
Jonathan Inch <jonathaninch@hotmail.com>
2022-12-23 13:09:17 +0000

    V4 info / links added to home page

---
+++ app/views/ver4/routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
+
+// Job library entry
+
+
+module.exports = router
