fe6ba0b9eca8e6d9099ed982ffdb88f2bf0136b5
Jonathan Inch <jonathaninch@hotmail.com>
2023-02-13 11:15:11 +0000

    V5R backup

---
+++ app/views/previous-versions/recruiter-ver5/routes.js
@@ -0,0 +1,12 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes here
+
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
